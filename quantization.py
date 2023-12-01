efc3ee85d40bf5c196d773d2c11c36b4b9e69079
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 13:55:58 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -1,13 +1,9 @@
 from transformers import AutoTokenizer, pipeline
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
 from optimum.onnxruntime import ORTOptimizer, OptimizationConfig
-from transformers import PushToHubCallback
 
-save_dir = "./model"
 model_id = "VietAI/vit5-large-vietnews-summarization"
-ORTModelForSeq2SeqLM.from_pretrained(save_dir=save_dir, model_id, export=True)
+model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-push_to_hub_callback = PushToHubCallback(
-    output_dir=save_dir, tokenizer=tokenizer, hub_model_id="tuananhhmx4/vi5Onnxruntime"
-)
+model.push_to_hub("vi5Onnxruntime")
