82bb8c61087177591b1f289ed19a9453c5e26d64
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 13:21:12 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -5,3 +5,5 @@ from optimum.onnxruntime import ORTOptimizer, OptimizationConfig
 model_id = "VietAI/vit5-large-vietnews-summarization"
 model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model.push_to_hub("tuananhhmx4/vi5Onnxruntime")
