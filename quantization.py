88b73327ce2e4fcc6bebedb58818b0fe38b75562
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 13:34:50 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -6,4 +6,4 @@ model_id = "VietAI/vit5-large-vietnews-summarization"
 model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-model.push_to_hub("tuananhhmx4/vi5Onnxruntime")
+model.push_to_hub("tuananhhmx4/vOnnxruntime")
