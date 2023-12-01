c72fdb9cc82bf664ac35faaec0a3a85b3118f359
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 15:50:20 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -9,25 +9,27 @@ import shutil
 access_token="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"
 login(token = access_token, add_to_git_credential=True)
 
-model_id = "VietAI/vit5-large-vietnews-summarization"
-model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
+model_id = "tuananhhmx4/vi5Onnxruntime"
+model = ORTModelForSeq2SeqLM.from_pretrained(model_id)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-try:
-    shutil.rmtree(file_utils.default_cache_path)
-except OSError as e:
-    print("Error: %s - %s." % (e.filename, e.strerror))
-
 save_dir = "./model"
-model.save_pretrained(save_dir)
 
+optimizer = ORTOptimizer.from_pretrained(model)
+optimization_config = OptimizationConfig(
+    optimization_level=2,
+    enable_transformers_specific_optimizations=True,
+    optimize_for_gpu=False,
+)
+
+optimizer.optimize(save_dir=save_dir, optimization_config=optimization_config)
 api = HfApi()
 
 # Upload all the content from the local folder to your remote Space.
 # By default, files are uploaded at the root of the repo
 api.upload_folder(
     folder_path="./model",
-    repo_id="tuananhhmx4/vi5Onnxruntime",
+    repo_id="tuananhhmx4/optimizeOnnx",
     repo_type="model",
 )
 
