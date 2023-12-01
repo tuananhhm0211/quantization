17e6cde05ce0348e16a6790dd6c1e5e8788dbeb7
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 14:42:53 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -4,7 +4,7 @@ from optimum.onnxruntime import ORTOptimizer, OptimizationConfig
 from huggingface_hub import login
 
 access_token="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"
-login(token = access_token)
+login(token = access_token, add_to_git_credential=True)
 
 model_id = "VietAI/vit5-large-vietnews-summarization"
 model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
