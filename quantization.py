2d88340a8155e4ac454cc510052868aae181e29a
tuananhhm0211 <78582842+tuananhhm0211@users.noreply.github.com>
2023-12-01 14:27:59 +0700

    Update quantization.py

--- quantization.py
+++ quantization.py
@@ -1,10 +1,14 @@
 from transformers import AutoTokenizer, pipeline
 from optimum.onnxruntime import ORTModelForSeq2SeqLM
 from optimum.onnxruntime import ORTOptimizer, OptimizationConfig
+from huggingface_hub import login
+
+access_token="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"
+login(token = access_token)
 
 model_id = "VietAI/vit5-large-vietnews-summarization"
 model = ORTModelForSeq2SeqLM.from_pretrained(model_id, export=True)
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 access_token="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"
-model.push_to_hub("vi5Onnxruntime", token=access_token)
+model.push_to_hub("vi5Onnxruntime")
